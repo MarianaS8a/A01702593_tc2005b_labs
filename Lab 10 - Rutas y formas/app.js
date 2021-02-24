10f4a7ee1d724016735adbb96a38d790e41882a3
Mariana Soto <a01702593@itesm.mx>
2021-02-23 21:57:29 -0600

    3 paginas

--- Lab 10 - Rutas y formas/app.js
+++ Lab 10 - Rutas y formas/app.js
@@ -1,22 +1,55 @@
 const http = require('http');
+
+const ropa = ["Jeans", "Blusa", "Sudadera", "Gorra"];
 const server = http.createServer((request, response) =>{
     if (request.url === "/inicio"){
         response.setHeader('Content-Type', 'text/html');
         response.write("<html>");
         response.write('<head><meta charset="UTF-8"><title>Servidor node</title></head>');
         response.write("<body><h1>Bienvenido a la tienda RECON</h1></body>");
-        response.write('<html class=no-js lang=es> <head> <meta charset=utf-8 /> <meta http-equiv=x-ua-compatible content="ie=edge"> <meta name=viewport content="width=device-width, initial-scale=1.0"> <title>Foundation for Sites</title> <link rel=stylesheet href="assets/css/foundation.css"> <link rel=stylesheet href="assets/css/main.css"> </head> <body> <header> <span class=title-bar-title>Ropa y accesorios</span><br></header> <main> <div class=grid-container> <div class="grid-x grid-margin-x align-stretch" data-equalizer data-equalize-by-row=true> <div class="cell small-1 medium-2 large-3" data-equalizer-watch> <div class=card> <div class=card-divider> <h3 id=titulo_blusa>Blusa roja</h3> </div> <div class=card-image> <img src="//www.belagoom.com/wp-content/uploads/2019/05/Blusa-roja-manga-volante.jpg" alt="Foto de blusa"> </div> <div class=card-section><p>Compra esta maravillosa blusa talla mediana.</p><h4>$250</h4> <h4>Cantidad:</h4> <select id=blusa> <option value=0>0</option> <option value=1>1</option> <option value=2>2</option> <option value=3>3</option> <option value=4>4</option> <option value=5>5</option> </select> </div> </div> </div> <div class="cell small-1 medium-2 large-3" data-equalizer-watch> <div class=card> <div class=card-divider> <h3 id=titulo_collar>Collar dorado</h3> </div> <img src="//resources.sears.com.mx/medios-plazavip/fotos/productos_sears1/original/3036812.jpg" alt="Foto de collar"> <div class=card-section><br><br><br><p>Destaca con este collar baÃ±ado en 2k de oro.</p><h4>$400</h4> <h4>Cantidad:</h4> <select id=collar> <option value=0>0</option> <option value=1>1</option> <option value=2>2</option> <option value=3>3</option> <option value=4>4</option> <option value=5>5</option> </select> </div> </div> </div> <div class="cell small-1 medium-2 large-3" data-equalizer-watch> <div class=card> <div class=card-divider> <h3 id=titulo_pants>Pants Puma</h3> </div> <img src="//resources.sears.com.mx/medios-plazavip/fotos/productos_sears1/original/3013246.jpg" alt="Foto de pants"> <div class=card-section><br><br><p>Usa estos pants para sentirte cÃ³modo o cÃ³moda esta cuarentena.</p><h4>$400</h4> <h4>Cantidad:</h4> <select id=pants> <option value=0>0</option> <option value=1>1</option> <option value=2>2</option> <option value=3>3</option> <option value=4>4</option> <option value=5>5</option> </select> </div> </div> </div> <div class="cell small-1 medium-2 large-3"> <div class=card> <div class=card-divider> <h3>Mi carrito</h3> </div> <div class=card-section><p id=product1></p><p id=product2></p><p id=product3></p><h4 id=total>Carrito vacÃ­o</h4> </div> </div> </div> </div> </div> </main> <footer><br><br><p>Material design: es un estándar para el diseño web y apps. Además, es un sistema de diseño desarrollado por Google para desarrollar apps responsivas, usables y escalables.</p><br><h3>Referencias</h3><p>Davidov, S. (2021). What Is Material Design? Definition, Uses, and Examples | Elementor. Retrieved 16 February 2021, from https://elementor.com/blog/what-is-material-design/</p></footer> <script src="assets/js/productos.js"></script> <script src="assets/js/vendor.js"></script> <script src="assets/js/foundation.js"></script> </body> </html>')
+        response.write("<h3>Artículos disponibles:</h3><ul>"); 
         
+        ropa.forEach(element => {
+            response.write( "<li>" + element + "</li>");
+        });
+
+        response.write("</ul");
+        //response.write('<form action= "add-product" method= "GET"><input type="submit" name="Add product"></form>');
+        response.write("</html>");
+        response.end();
+    }
+    
+    
+    else if(request.url ==="/add-product" && request.method === "GET"){
+        response.setHeader('Content-Type', 'text/html');
+        response.write("<html>");
+        response.write('<head><meta charset="UTF-8"><title>Servidor node</title></head>');
+        response.write("<body><h1>Añade tu producto</h1></body>");
+        response.write('<form action= "add-product" method= "POST"><input type="text" name= "nombre"><input type="submit" value= "Guardar producto"></form>');
+        response.write("</html>");
+    }
+
+    else if(request.url ==="/add-product" && request.method === "POST"){
+        let datos = [];
+        request.on('data',function(dato){
+            datos.push(dato);
+        })
+        request.on('end', function (){
+        let strdatos = Buffer.concat(datos).toString();
+        let producto = strdatos.split('=')[1];
+        ropa.push(producto);
+        })
+        response.writeHead(301,{Location:'/inicio'});
+    }
+
+    else if(request.url ==="/about-us"){
+        response.setHeader('Content-Type', 'text/html');
+        response.write("<html>");
+        response.write('<head><meta charset="UTF-8"><title>Acerca de nosotros</title></head>');
+        response.write("<h1>Acerca de nostros</h1>");
+        response.write("<body><h3>RECON se fundó en 1960 y este año celebra su 81 aniversario desde que comenzamos siendo una pequeña empresa local queretana.</h3></body>");
     }
 /*
-    else if(request.url ==="/sudadera"){
-
-    }
-
-    else if(request.url ==="/blusa"){
-
-    }
-
     else{
 
     }*/
